3fb1c983788747984bd09a0bbd4275fb5d36b73e
Thanon <fourdat.444@gmail.com>
2026-03-18 11:51:14 +0700

    แก้ผิด

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,18 +2,6 @@ import "./globals.css";
 import Header from "./components/ui/Header";
 import Footer from "./components/ui/Footer";
 
-<<<<<<< HEAD
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-
 export const metadata = {
   title: "ราชวิทยาลัยเภสัชกรรมแห่งประเทศไทย",
   description: "The Royal College of Pharmacy of Thailand",
